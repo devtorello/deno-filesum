55f6b67725b144e0d9a915cc4faba4e5ffe8c1cb
vitorellotati <vitorellotts@gmail.com>
2022-08-04 14:06:33 -0300

    ♻️ refactor: move files to logic folder

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,3 @@
 export { randomNumber } from 'https://deno.land/x/random_number@2.0.0/mod.ts';
+export { dirname } from "https://deno.land/std@0.150.0/path/mod.ts";
 export * from "https://deno.land/std@0.150.0/fs/mod.ts";
