e4545085c34c50663ef0dd2092708a8891f9f745
vitorellotati <vitorellotts@gmail.com>
2022-08-04 14:17:18 -0300

    ♻️ refactor: add console log to make the process more visible

--- src/distributor.ts
+++ src/distributor.ts
@@ -4,6 +4,7 @@ export function sumDistributedFileNumbers(line: string, name: string): Promise<n
 	return new Promise((resolve) => {
 		const worker = new Worker(createWorkerURL('./workers.ts'), { type: 'module' });
 		worker.addEventListener('message', (message) => {
+			console.log(`Total of ${message.data.sum} returned for worker ${message.data.name}`);
 			resolve(message.data.sum);
 		});
 		worker.postMessage({ line, name });
